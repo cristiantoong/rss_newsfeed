c2cc4943e44d1f494a83625c88d6440e30d6d7aa
cristian <cristiantoong@gmail.com>
2019-09-16 21:06:08 +0800

    Added Files

--- news_feed/urls.py
+++ news_feed/urls.py
@@ -4,6 +4,6 @@ from rss.views import rss_view, search_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rss/', rss_view, name='rss'),
+    path('', rss_view, name='rss'),
     path('search_url/', search_url),
 ]
